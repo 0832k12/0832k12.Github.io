5743515faef1887fc872e971116792940944de69
0832k12 <0832k12@gmail.com>
2023-09-27 17:34:57 +0800

    ⚡重新添加一些东西

---
+++ Extensions/function.js
@@ -0,0 +1,97 @@
+(function (Scratch) {
+    'use strict';
+
+    class evaal {
+        getInfo() {
+            return {
+                id: 'function0832',
+                name: 'Function',
+                color1: '#f54242',
+                color2: '#f54242',
+                color3: '#f54242',
+                blocks: [
+                    {
+                        opcode: 'function',
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: '声明一个function [a]',
+                        arguments: {
+                            a: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "return 1+1;"
+                            }
+                        }
+                    },
+                    {
+                        opcode: 'dofunctioncm',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: '执行Function [a]',
+                        arguments: {
+                            a: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: ""
+                            }
+                        }
+                    },
+                    {
+                        opcode: 'dofunction',
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: '执行Function [a]',
+                        arguments: {
+                            a: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: ""
+                            }
+                        }
+                    }
+                ]
+            };
+        }
+        function({ a }) {
+            try {
+                const result = new Function(a);
+                return result;
+            }
+            catch (error) {
+                if (0) {
+                    console.error(error);
+                    return "Error: " + error.message;
+                }
+                else {
+                    return "";
+                }
+            }
+        }
+        dofunction({ a }) {
+            try {
+                const result = a();
+                return result;
+            }
+            catch (error) {
+                if (0) {
+                    console.error(error);
+                    return "Error: " + error.message;
+                }
+                else {
+                    return "";
+                }
+            }
+        }
+        dofunctioncm({ a }) {
+            try {
+                const result = a();
+                return result;
+            }
+            catch (error) {
+                if (0) {
+                    console.error(error);
+                    return "Error: " + error.message;
+                }
+                else {
+                    return "";
+                }
+            }
+        }
+    }
+
+    Scratch.extensions.register(new evaal());
+})(Scratch);
---
+++ rxFS/index.md
@@ -0,0 +1,4 @@
+## rxFS Extension For TurboWarp
+这个程序是一个文件系统！
+基于文件的检索功能，更改文件位置只需要更改头部即可。
+[查看rxFS所使用的许可证](https://0832k12.github.io/rxLi/)
